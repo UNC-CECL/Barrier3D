2d5cb7f7f72a00a570879a5989a56de8c2fee206
mcflugen <mcflugen@gmail.com>
2020-06-02 11:51:12 -0600

    update license and entry point

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     url="https://github.com/anardek/Barrier3D",
     classifiers=[
         "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GPLv3 License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
@@ -22,11 +22,9 @@ setup(
     ],
     install_requires=open("requirements.txt", "r").read().splitlines(),
     setup_requires=[],
+    include_package_data=True,
     packages=find_packages(),
-    # entry_points={
-    #     "console_scripts": ["compact=compaction.cli:compact"],
-    #     "landlab.components": ["Compact=compaction.landlab:Compact"],
-    # },
+    entry_points={"console_scripts": ["b3d=barrier3d.cli:barrier3d"]},
     # version=versioneer.get_version(),
     # cmdclass=versioneer.get_cmdclass(),
 )
