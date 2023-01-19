05a5219411a012f58a230e441d1e9fe0ebfbe24f
Lexi Van Blunk <agvanblu@ncsu.edu>
2023-01-19 14:22:04 -0500

    lexi's run file for b3d

--- scripts/Lexi_runB3D.py
+++ scripts/Lexi_runB3D.py
@@ -3,12 +3,13 @@ from barrier3d import Barrier3d
 from matplotlib import pyplot as plt
 import numpy as np
 import os
-from cascade.outwasher_reorg_b3d import plot_ElevAnimation
+# from cascade.outwasher_reorg_b3d import plot_ElevAnimation
 
 
-path = "C:/Users/Lexi/Documents/Research/Outwasher/Output/"
-runID = "b3d_domains"
-newpath = path + runID + "/"
+# path = "C:/Users/Lexi/Documents/Research/Outwasher/Output/"
+# runID = "b3d_domains"
+# newpath = path + runID + "/"
+# newpath = "D:/NC State/Outwasher/Output/b3d_domains/"
 
 b3d = Barrier3d.from_yaml("tests/test_params/")
 
@@ -16,46 +17,48 @@ b3d = Barrier3d.from_yaml("tests/test_params/")
 # AvgSlope = b3d._BermEl / 20
 # print("B3D avg slope of the interior = ", AvgSlope)
 
-directory = "C:/Users/Lexi/Documents/Research/Outwasher/Output/b3d_domains/"
+directory = "D:/NC State/Outwasher/Output/b3d_domains/"
 if not os.path.exists(directory):
     os.makedirs(directory)
 os.chdir(directory)
 # hours = b3d._TMAX
 # elevs = np.zeros([b3d._TMAX, b3d._DomainWidth, b3d._BarrierLength])
-for time_step in range(1, b3d._TMAX):
+# for time_step in range(1, b3d._TMAX):
+for time_step in range(1, 125):
     b3d.update()  # update the model's main time loop
     b3d.update_dune_domain()  # now update the dune domain and increase time by one year
     int_domain = np.flip(b3d.InteriorDomain)
     os.chdir(directory)
-    newpath = "Elevations/"
+    newpath = "D:/NC State/Outwasher/Output/b3d_domains/Elevation_data/"
     if not os.path.exists(newpath):
         os.makedirs(newpath)
     os.chdir(newpath)
-
-    AnimateDomain = int_domain
-
-    # Plot and save
-    plt.rcParams.update({"font.size": 15})
-    elevFig1 = plt.figure(figsize=(15, 7))
-    ax = elevFig1.add_subplot(111)
-    cax = ax.matshow(
-        AnimateDomain,
-        # origin="upper",
-        cmap="terrain",
-        vmin=-0.3, vmax=0.4
-    )  # , interpolation='gaussian') # analysis:ignore
-    ax.xaxis.set_ticks_position("bottom")
-    elevFig1.colorbar(cax)
-    plt.xlabel("Alongshore Distance (dam)")
-    plt.ylabel("Cross-Shore Distance (dam)")
-    plt.title("Elevation (dam)")
-    plt.tight_layout()
-    timestr = "Time = " + str(time_step) + " hrs"
-    plt.text(1, 1, timestr)
-    name = "elev_" + str(time_step)
-    elevFig1.savefig(name, facecolor='w')  # dpi=200
-    plt.close(elevFig1)
-    # Print time step to screen
+    name = "domain_" + str(time_step)
+    np.save(newpath + name, int_domain)
+    # AnimateDomain = int_domain
+    #
+    # # Plot and save
+    # plt.rcParams.update({"font.size": 15})
+    # elevFig1 = plt.figure(figsize=(15, 7))
+    # ax = elevFig1.add_subplot(111)
+    # cax = ax.matshow(
+    #     AnimateDomain,
+    #     # origin="upper",
+    #     cmap="terrain",
+    #     vmin=-0.3, vmax=0.4
+    # )  # , interpolation='gaussian') # analysis:ignore
+    # ax.xaxis.set_ticks_position("bottom")
+    # elevFig1.colorbar(cax)
+    # plt.xlabel("Alongshore Distance (dam)")
+    # plt.ylabel("Cross-Shore Distance (dam)")
+    # plt.title("Elevation (dam)")
+    # plt.tight_layout()
+    # timestr = "Time = " + str(time_step) + " hrs"
+    # plt.text(1, 1, timestr)
+    # name = "elev_" + str(time_step)
+    # elevFig1.savefig(name, facecolor='w')  # dpi=200
+    # plt.close(elevFig1)
+    # # Print time step to screen
     print("\r", "Time Step: ", time_step, end="\n")
 
 print("\r", "Qow: ", b3d._QowTS, end="\n")
