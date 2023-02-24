d26d2aee36bb8f9343707dab338e96c9e5b52e2a
mcflugen <mcflugen@gmail.com>
2023-02-24 10:14:04 -0700

    store the version in _version.py

--- barrier3d/__init__.py
+++ barrier3d/__init__.py
@@ -1,11 +1,9 @@
-import pkg_resources
-
+from ._version import __version__
 from .barrier3d import Barrier3d
 from .bmi import Barrier3dBmi
 from .configuration import Barrier3dConfiguration
 from .load_input import load_inputs
 
-__version__ = pkg_resources.get_distribution("barrier3d").version
-__all__ = ["Barrier3d", "Barrier3dBmi", "Barrier3dConfiguration", "load_inputs"]
-
-del pkg_resources
+__all__ = [
+    "__version__", "Barrier3d", "Barrier3dBmi", "Barrier3dConfiguration", "load_inputs"
+]
