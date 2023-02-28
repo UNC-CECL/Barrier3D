941c7903e1d47686bcb11f10c16696f14f38a2ee
mcflugen <mcflugen@gmail.com>
2023-02-28 10:39:10 -0700

    clean up version test; require an old numpy, mark as slow

--- tests/test_versions.py
+++ tests/test_versions.py
@@ -1,47 +1,63 @@
+import os
+import pathlib
+from packaging.version import Version
+
 import numpy as np
 from numpy.testing import assert_array_almost_equal
-import os
+import pytest
+
 from barrier3d import Barrier3dBmi
 
-DATA_DIR = os.path.abspath(os.path.join(__file__, "../../version1_local_copy"))
-# datadir_V1 = "version1_local_copy/"
-# bmi_datadir = "tests/test_versions/"
-
-
 # Version 1.0 (original version written by Ian Reeves) ------------------------------
-# NOTE: putting this in a function didn't work
-DuneDomain = []
-x_s_TS = []
-ShorelineChangeTS = []
-s_sf_TS = []
-QowTS = []
-
-exec(open(DATA_DIR + "/Barrier3D.py").read())
+DATA_DIR = pathlib.Path(__file__).absolute().parent.parent / "version1_local_copy"
 
 
-def test_BMI_against_V1(datadir):
-    """
-    check that the BMI and Version 1 of Barrier3D (by Ian Reeves) are equivalent
-    """
+def run_old_version():
+    from importlib.machinery import SourceFileLoader
+
+    mod = SourceFileLoader("v1", str(DATA_DIR / "Barrier3D.py")).load_module()
+
+    return {
+        k: mod.__dict__[k]
+        for k in ["DuneDomain", "x_s_TS", "ShorelineChangeTS", "s_sf_TS", "QowTS"]
+    }
+
+
+@pytest.mark.skipif(
+    Version(np.__version__) >= Version("1.24"),
+    reason="numpy<1.24 required for barrier3d v1",
+)
+@pytest.mark.slow
+def test_BMI_against_V1(tmpdir, datadir):
+    """check that the BMI and Version 1 of Barrier3D (by Ian Reeves) are equivalent."""
+    with tmpdir.as_cwd():
+        expected = run_old_version()
 
     # Version 2.0 and beyond (contains a BMI) ------------------------------
-    # NOTE: putting this in a separate function also didn't work (b/c of pytest usage of datadir)
-    barrier3d = Barrier3dBmi()
-    barrier3d.initialize(str(datadir) + "/barrier3d-default-parameters.yaml")
+    with tmpdir.as_cwd():
+        barrier3d = Barrier3dBmi()
+        barrier3d.initialize(datadir / "barrier3d-default-parameters.yaml")
 
-    # increase time step
-    for time_step in range(1, barrier3d._model._TMAX):
-        barrier3d.update()
+        # increase time step
+        for time_step in range(1, barrier3d._model._TMAX):
+            barrier3d.update()
 
-    # assert np.all(barrier3d._model._DuneDomain[-1, :, 0] == DuneDomain[-1, :, 0])  # dune height over time
-    # assert_array_almost_equal(barrier3d._model._DuneDomain[-1, :, 0], DuneDomain[-1, :, 0])  # neither of these worked
-    assert np.all(
-        barrier3d._model._x_s_TS == x_s_TS
-    )  # shoreline change time series, BMI consistently less
-    assert np.all(
-        barrier3d._model._ShorelineChangeTS == ShorelineChangeTS  #
-    )  # dune migration time series
-    assert np.all(barrier3d._model._s_sf_TS == s_sf_TS)  # shoreface slope time series
-    assert np.all(
-        barrier3d._model._QowTS == QowTS
-    )  # because we are producing less overwash
+    # dune height over time
+    # assert np.all(barrier3d._model._DuneDomain[-1, :, 0] == DuneDomain[-1, :, 0])
+
+    # neither of these worked
+    # assert_array_almost_equal(
+    #     barrier3d._model._DuneDomain[-1, :, 0], DuneDomain[-1, :, 0]
+    # )
+
+    # shoreline change time series, BMI consistently less
+    assert np.all(barrier3d._model._x_s_TS == expected["x_s_TS"])
+
+    # dune migration time series
+    assert np.all(barrier3d._model._ShorelineChangeTS == expected["ShorelineChangeTS"])
+
+    # shoreface slope time series
+    assert np.all(barrier3d._model._s_sf_TS == expected["s_sf_TS"])
+
+    # because we are producing less overwash
+    assert np.all(barrier3d._model._QowTS == expected["QowTS"])
